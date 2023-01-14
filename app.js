07ba05c4b2264831538ae1a07fb31486f819c22c
Sonal Agrawal <sonalfeb7@gmail.com>
2023-01-14 12:06:55 +0530

    Add Router

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const bodyparser = require("body-parser");
+const posts = require("./routes/post");
 const app = express();
 app.use(bodyparser.json());
 const PORT = 5000;
+app.use("/posts", posts);
+app.get("/", (req, res) => {
+  res.send("Hello from Snapbizz");
+});
 app.listen(PORT, () => {
   console.log(`Server Rinning on PORt - http://localhost:${PORT}`);
 });
